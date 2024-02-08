b784d1cbb489a56c01526af218a3a1de8027f6b5
albaferrum <cemakdemir99@gmail.com>
2024-02-09 02:54:31 +0300

    stylesheet improvement

--- client/money-tracker-client/src/App.js
+++ client/money-tracker-client/src/App.js
@@ -5,6 +5,59 @@ function App() {
   return (
     <main>
        <h1>$400<span>,00</span></h1>
+       <form>
+        <div className='basic'>
+          <input type="text" placeholder = {'+200 new computer'}></input>
+          <input type="datetime-local"></input>
+        </div>
+        <div className='description' placeholder={'description'}>
+         <input type="text"></input>
+        </div>
+        <button type='submit'>Add new transaction</button>
+       </form>
+       
+       <div className='transactions'>
+          <div className='transaction'>
+            <div className='left'>
+              <div className='name'>New Samsung TV</div>
+              <div className='description'>it was time for new tv.</div>
+            </div>
+            <div className='right'>
+              <div className='price red'>-$500</div>
+              <div className='datetime'>2022-12-18 15:45</div>
+            </div>
+          </div>
+          <div className='transaction'>
+            <div className='left'>
+              <div className='name'>New Samsung TV</div>
+              <div className='description'>it was time for new tv.</div>
+            </div>
+            <div className='right'>
+              <div className='price green'>$500</div>
+              <div className='datetime'>2022-12-18 15:45</div>
+            </div>
+          </div>
+          <div className='transaction'>
+            <div className='left'>
+              <div className='name'>New Samsung TV</div>
+              <div className='description'>it was time for new tv.</div>
+            </div>
+            <div className='right'>
+              <div className='price red'>-$500</div>
+              <div className='datetime'>2022-12-18 15:45</div>
+            </div>
+          </div>
+          <div className='transaction'>
+            <div className='left'>
+              <div className='name'>New Samsung TV</div>
+              <div className='description'>it was time for new tv.</div>
+            </div>
+            <div className='right'>
+              <div className='price green'>$500</div>
+              <div className='datetime'>2022-12-18 15:45</div>
+            </div>
+          </div>
+       </div>
     </main>
 
   );
